cc38aaa2425916783f64cfe094f7c3640f3809e2
Ann Dolbina <dolbinaann@gmail.com>
2023-05-19 18:38:05 +0300

    add lessons

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
-import { StrictMode } from 'react';
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
-import 'modern-normalize';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
 
+
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter basename="/react_lesson5">
-      <App />
-    </BrowserRouter>
-  </StrictMode>
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
+
+
